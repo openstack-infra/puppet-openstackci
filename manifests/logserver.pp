e8106a86e6f2507a44e2ccc7d2f137bd6b413a6f
Ramy Asselin <ramy.asselin@hp.com>
2015-03-24 15:50:51 -0700

    Create a log server like http://logs.openstack.org
    Copy the log server templates & files used in:
    https://git.openstack.org/cgit/openstack-infra/system-config/tree/modules/openstack_project
    
    Refactor the log server portion out of:
    https://git.openstack.org/cgit/openstack-infra/system-config/tree/modules/openstack_project/manifests/static.pp
    
    Additional modifications:
    1. Add protections for portions that are likely to conflict.
    2. Update os_loganalyzer install to use pip instead of python
    
    Related system-config change: I390f6ea8aaace276d211d55a0e17f25dd6ae26b5
    
    Change-Id: Ib8476df83b7c5491158fa3cab588213be60fa2ab

---
+++ manifests/logserver.pp
@@ -0,0 +1,183 @@
+# Copyright (c) 2012-2015 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+# implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# == Class: openstackci::logserver
+#
+class openstackci::logserver (
+  $domain,
+  $jenkins_ssh_key,
+  $swift_authurl = '',
+  $swift_user = '',
+  $swift_key = '',
+  $swift_tenant_name = '',
+  $swift_region_name = '',
+  $swift_default_container = '',
+) {
+
+  if ! defined(Class['::jenkins::jenkinsuser']) {
+    class { '::jenkins::jenkinsuser':
+      ssh_key => $jenkins_ssh_key,
+    }
+  }
+
+  include apache
+  include apache::mod::wsgi
+
+  if ! defined(A2mod['rewrite']) {
+    a2mod { 'rewrite':
+      ensure => present,
+    }
+  }
+
+  if ! defined(A2mod['proxy']) {
+    a2mod { 'proxy':
+      ensure => present,
+    }
+  }
+
+  if ! defined(A2mod['proxy_http']) {
+    a2mod { 'proxy_http':
+      ensure => present,
+    }
+  }
+
+  if ! defined(File['/srv/static']) {
+    file { '/srv/static':
+      ensure => directory,
+    }
+  }
+
+  apache::vhost { "logs.${domain}":
+    port     => 80,
+    priority => '50',
+    docroot  => '/srv/static/logs',
+    require  => File['/srv/static/logs'],
+    template => 'openstackci/logs.vhost.erb',
+  }
+
+  apache::vhost { "logs-dev.${domain}":
+    port     => 80,
+    priority => '51',
+    docroot  => '/srv/static/logs',
+    require  => File['/srv/static/logs'],
+    template => 'openstackci/logs-dev.vhost.erb',
+  }
+
+  file { '/srv/static/logs':
+    ensure  => directory,
+    owner   => 'jenkins',
+    group   => 'jenkins',
+    require => User['jenkins'],
+  }
+
+  file { '/srv/static/logs/robots.txt':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    source  => 'puppet:///modules/openstackci/disallow_robots.txt',
+    require => File['/srv/static/logs'],
+  }
+
+  package { 'keyring':
+    ensure   => 'latest',
+    provider => 'pip',
+  }
+
+  vcsrepo { '/opt/os-loganalyze':
+    ensure   => latest,
+    provider => git,
+    revision => 'master',
+    source   => 'https://git.openstack.org/openstack-infra/os-loganalyze',
+    require  => Package['keyring'],
+  }
+
+  exec { 'install_os-loganalyze':
+    command     => 'pip install .',
+    cwd         => '/opt/os-loganalyze',
+    path        => '/usr/local/bin:/usr/bin:/bin/',
+    refreshonly => true,
+    subscribe   => Vcsrepo['/opt/os-loganalyze'],
+  }
+
+  file { '/etc/os_loganalyze':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Vcsrepo['/opt/os-loganalyze'],
+  }
+
+  file { '/etc/os_loganalyze/wsgi.conf':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'www-data',
+    mode    => '0440',
+    content => template('openstackci/os-loganalyze-wsgi.conf.erb'),
+    require => File['/etc/os_loganalyze'],
+  }
+
+  vcsrepo { '/opt/devstack-gate':
+    ensure   => latest,
+    provider => git,
+    revision => 'master',
+    source   => 'https://git.openstack.org/openstack-infra/devstack-gate',
+  }
+
+  file { '/srv/static/logs/help':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => File['/srv/static/logs'],
+  }
+
+  file { '/srv/static/logs/help/tempest-logs.html':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    source  => 'file:///opt/devstack-gate/help/tempest-logs.html',
+    require => [File['/srv/static/logs/help'], Vcsrepo['/opt/devstack-gate']],
+  }
+
+  file { '/srv/static/logs/help/tempest-overview.html':
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0444',
+    source  => 'file:///opt/devstack-gate/help/tempest-overview.html',
+    require => [File['/srv/static/logs/help'], Vcsrepo['/opt/devstack-gate']],
+  }
+
+  file { '/usr/local/sbin/log_archive_maintenance.sh':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0744',
+    source => 'puppet:///modules/openstackci/log_archive_maintenance.sh',
+  }
+
+  cron { 'gziprmlogs':
+    user        => 'root',
+    minute      => '0',
+    hour        => '7',
+    weekday     => '6',
+    command     => 'bash /usr/local/sbin/log_archive_maintenance.sh',
+    environment => 'PATH=/usr/bin:/bin:/usr/sbin:/sbin',
+    require     => File['/usr/local/sbin/log_archive_maintenance.sh'],
+  }
+
+}
