aaf32bcbbf2bcbc2cd41f160d742645bc90f9745
Spencer Krum <nibz@cat.pdx.edu>
2015-05-21 15:38:04 -0700

    Add beaker boilerplate to openstackci
    Updated for suggestions from crinkle. Left spec_helper_acceptance alone
    because jeblair's change is the right place to make that modification.
    
    Change-Id: Ic1aa2a0a78ea74661e95399e1e6393af5cf687b0

---
+++ spec/acceptance/basic_spec.rb
@@ -0,0 +1,23 @@
+require 'spec_helper_acceptance'
+
+describe 'basic openstackci' do
+
+  context 'default parameters' do
+
+    it 'should work with no errors' do
+
+      base_path = File.dirname(__FILE__)
+      pp_path = File.join(base_path, 'fixtures', 'default.pp')
+      pp = read(pp_path)
+
+      # Run it twice and test for idempotency
+      apply_manifest(pp, :catch_failures => true)
+      apply_manifest(pp, :catch_changes => true)
+    end
+
+    describe port(80) do
+      it { is_expected.to be_listening.with('tcp') }
+    end
+
+  end
+end
