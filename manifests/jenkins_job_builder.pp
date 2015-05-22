b09333154f1e158b9483c7f018f60cc458abf2db
Jaume Devesa <devvesa@gmail.com>
2015-05-22 02:12:57 +0200

    Add Jenkins Job Builder manifest
    Some third-party vendors may want to leverage the Jenkins Job Builder
    library, some may not. Put it in a separate manifest from the previous
    `system-config`'s `jenkins.pp` file.
    
    Change-Id: Ibc7a46a8cb364b9e64b1fddb6b4f9df5a9e1fdd5

---
+++ manifests/jenkins_job_builder.pp
@@ -0,0 +1,26 @@
+# == Class: openstackci::jenkins_job_builder
+#
+class openstackci::jenkins_job_builder (
+  $vhost_name = $::fqdn,
+  $jenkins_jobs_username = 'gerrig',
+  $jenkins_jobs_password = '',
+  $jenkins_git_url = 'https://git.openstack.org/openstack-infra/jenkins-job-builder',
+  $jenkins_git_revision = 'master',
+  $project_config_repo = ''
+) {
+
+  class { 'project_config':
+    url  => $project_config_repo,
+  }
+
+  class { '::jenkins::job_builder':
+    jenkins_jobs_update_timeout => 1200,
+    url                         => "https://${vhost_name}/",
+    username                    => $jenkins_jobs_username,
+    password                    => $jenkins_jobs_password,
+    git_revision                => $jenkins_git_revision,
+    git_url                     => $jenkins_git_url,
+    config_dir                  => $::project_config::jenkins_job_builder_config_dir,
+    require                     => $::project_config::config_dir,
+  }
+}
