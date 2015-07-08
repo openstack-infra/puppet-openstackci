7d400578a77dad0f84d6f835ee89c9cc27fa2180
Mikhail S Medvedev <mmedvede@us.ibm.com>
2015-07-08 18:49:22 -0500

    Add generic logstash manifests
    Copy logstash, logstash_worker, subunit_worker, elasticsearch_node
    manifests from system-config openstack_project module and refactor
    (expose class parameters).
    
    Additional changes:
     * Get rid of logstash-indexer.default in favor of setting content
       inline. Parametrize the JAVA_ARGS setting.
     * Expose more parameters.
     * Replace declaration of several log processor workers with
       an array parameter.
    
    Change-Id: I690a3e5f8ff5cee5716ee7dd3bbb99091b0a8ca3

---
+++ manifests/logstash_worker.pp
@@ -0,0 +1,67 @@
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+
+# == Class: openstackci::logstash_worker
+#
+# Logstash indexer worker glue class.
+#
+class openstackci::logstash_worker (
+  $elasticsearch_nodes = [],
+  $log_processor_config,
+  $heap_size = '1g',
+  $version = '0.90.9',
+  # Increase the indexer max heap size to twice the default.
+  # Default is 25% of memory or 1g whichever is less.
+  $indexer_java_args = '-Xmx2g',
+  $log_processor_workers = ['A', 'B', 'C', 'D',],
+  $es_gw_recover_after_nodes = '5',
+  $es_gw_recover_after_time = '5m',
+  $es_gw_expected_nodes = '6',
+  $es_discovery_min_master_nodes = '5',
+  $es_indices_cleanup_hour = '2',
+) {
+
+  file { '/etc/default/logstash-indexer':
+    ensure => present,
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    content => "JAVA_ARGS='${indexer_java_args}'"
+  }
+
+  class { 'logstash::indexer':
+    conf_template => 'openstackci/logstash/indexer.conf.erb',
+  }
+
+  include log_processor
+  log_processor::worker { $log_processor_workers:
+    config_file => $log_processor_config,
+  }
+
+  class { '::elasticsearch':
+    es_template_config => {
+      'gateway.recover_after_nodes'          => $es_gw_recover_after_nodes,
+      'gateway.recover_after_time'           => $es_gw_recover_after_time,
+      'gateway.expected_nodes'               => $es_gw_expected_nodes,
+      'discovery.zen.minimum_master_nodes'   => $es_discovery_min_master_nodes,
+      'discovery.zen.ping.multicast.enabled' => false,
+      'discovery.zen.ping.unicast.hosts'     => $elasticsearch_nodes,
+      'node.master'                          => false,
+      'node.data'                            => false,
+    },
+    heap_size          => $heap_size,
+    version            => $version,
+  }
+
+}
