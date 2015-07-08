38859d66e35312cc18b5b46cb2e0f8ae02431cb6
Mikhail S Medvedev <mmedvede@us.ibm.com>
2015-07-08 18:49:22 -0500

    Add generic logstash manifests
    Copy logstash, logstash_worker, subunit_worker, elasticsearch_node
    manifests from system-config openstack_project module and refactor
    (expose class parameters).
    
    Additional changes:
     * Get rid of logstash-indexer.default in favor of setting content
       inline. Parametrize the JAVA_ARGS setting.
     * Expose more parameters.
     * Replace declaration of several log processor workers with
       an array parameter.
    
    Change-Id: I690a3e5f8ff5cee5716ee7dd3bbb99091b0a8ca3

---
+++ manifests/elasticsearch_node.pp
@@ -0,0 +1,59 @@
+# Copyright 2013 Hewlett-Packard Development Company, L.P.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may obtain
+# a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+# WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+# License for the specific language governing permissions and limitations
+# under the License.
+#
+# == Class: openstackci::elasticsearch_node
+
+# Elasticsearch server glue class.
+#
+class openstackci::elasticsearch_node (
+  $discover_nodes = ['localhost'],
+  $heap_size = '30g',
+  $version = '0.90.9',
+  $es_gw_recover_after_nodes = '5',
+  $es_gw_recover_after_time = '5m',
+  $es_gw_expected_nodes = '6',
+  $es_discovery_min_master_nodes = '4',
+  $es_indices_cleanup_hour = '2',
+  $es_indices_cleanup_minute = '0',
+  $es_indices_cleanup_period = '10 days ago',
+) {
+
+  class { 'logstash::elasticsearch': }
+
+  class { '::elasticsearch':
+    es_template_config => {
+      'index.store.compress.stored'          => true,
+      'index.store.compress.tv'              => true,
+      'indices.memory.index_buffer_size'     => '33%',
+      'bootstrap.mlockall'                   => true,
+      'gateway.recover_after_nodes'          => $es_gw_recover_after_nodes,
+      'gateway.recover_after_time'           => $es_gw_recover_after_time,
+      'gateway.expected_nodes'               => $es_gw_expected_nodes,
+      'discovery.zen.minimum_master_nodes'   => $es_discovery_min_master_nodes,
+      'discovery.zen.ping.multicast.enabled' => false,
+      'discovery.zen.ping.unicast.hosts'     => $discover_nodes,
+    },
+    heap_size          => $heap_size,
+    version            => $version,
+  }
+
+  cron { 'delete_old_es_indices':
+    user        => 'root',
+    hour        => $es_indices_cleanup_hour,
+    minute      => $es_indices_cleanup_minute,
+    command     => "curl -sS -XDELETE \"http://localhost:9200/logstash-`date -d '${es_indices_cleanup_period}' +\%Y.\%m.\%d`/\" > /dev/null",
+    environment => 'PATH=/usr/bin:/bin:/usr/sbin:/sbin',
+  }
+
+}
