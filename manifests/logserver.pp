a16f3a99ca9a2424996ccd43ab8414d594aa1aab
Jeremy Stanley <fungi@yuggoth.org>
2015-10-22 19:27:48 +0000

    Add flexible HTTPS support for production vhost
    Change-Id: Id9dfcbbd2fe8cce8df8fcc230a133eb9ff35c9d1

--- manifests/logserver.pp
+++ manifests/logserver.pp
@@ -24,6 +24,13 @@ class openstackci::logserver (
   $swift_tenant_name = '',
   $swift_region_name = '',
   $swift_default_container = '',
+  $ssl = false,
+  $ssl_cert_file = '',
+  $ssl_cert_file_contents = '',
+  $ssl_key_file = '',
+  $ssl_key_file_contents = '',
+  $ssl_chain_file = '',
+  $ssl_chain_file_contents = '',
 ) {
 
   if ! defined(Class['::jenkins::jenkinsuser']) {
@@ -56,20 +63,137 @@ class openstackci::logserver (
     }
   }
 
-  ::httpd::vhost { "logs.${domain}":
-    port     => 80,
-    priority => '50',
-    docroot  => '/srv/static/logs',
-    require  => File['/srv/static/logs'],
-    template => 'openstackci/logs.vhost.erb',
+  if $ssl = true {
+    if ! defined(File['/etc/ssl/certs']) {
+      file { '/etc/ssl/certs':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0755',
+      }
+    }
+
+    if ! defined(File['/etc/ssl/private']) {
+      file { '/etc/ssl/private':
+        ensure => directory,
+        owner  => 'root',
+        group  => 'root',
+        mode   => '0700',
+      }
+    }
+
+    # To use the standard ssl-certs package snakeoil certificate, leave both
+    # $ssl_cert_file and $ssl_cert_file_contents empty. To use an existing
+    # certificate, specify its path for $ssl_cert_file and leave
+    # $ssl_cert_file_contents empty. To manage the certificate with puppet,
+    # provide $ssl_cert_file_contents and optionally specify the path to use for
+    # it in $ssl_cert_file.
+    if ($ssl_cert_file == '') and ($ssl_cert_file_contents == '') {
+      $cert_file = '/etc/ssl/certs/ssl-cert-snakeoil.pem'
+    } else {
+      if $ssl_cert_file == '' {
+        $cert_file = "/etc/ssl/certs/${::fqdn}.pem"
+      } else {
+        $cert_file = $ssl_cert_file
+      }
+      if $ssl_cert_file_contents != '' {
+        file { $cert_file:
+          ensure  => present,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => $ssl_cert_file_contents,
+          require => File['/etc/ssl/certs'],
+        }
+      }
+    }
+
+    # To use the standard ssl-certs package snakeoil key, leave both
+    # $ssl_key_file and $ssl_key_file_contents empty. To use an existing key,
+    # specify its path for $ssl_key_file and leave $ssl_key_file_contents empty.
+    # To manage the key with puppet, provide $ssl_key_file_contents and
+    # optionally specify the path to use for it in $ssl_key_file.
+    if ($ssl_key_file == '') and ($ssl_key_file_contents == '') {
+      $key_file = '/etc/ssl/private/ssl-cert-snakeoil.key'
+    } else {
+      if $ssl_key_file == '' {
+        $key_file = "/etc/ssl/private/${::fqdn}.key"
+      } else {
+        $key_file = $ssl_key_file
+      }
+      if $ssl_key_file_contents != '' {
+        file { $key_file:
+          ensure  => present,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0600',
+          content => $ssl_key_file_contents,
+          require => File['/etc/ssl/private'],
+        }
+      }
+    }
+
+    # To avoid using an intermediate certificate chain, leave both
+    # $ssl_chain_file and $ssl_chain_file_contents empty. To use an existing
+    # chain, specify its path for $ssl_chain_file and leave
+    # $ssl_chain_file_contents empty. To manage the chain with puppet, provide
+    # $ssl_chain_file_contents and optionally specify the path to use for it in
+    # $ssl_chain_file.
+    if ($ssl_chain_file == '') and ($ssl_chain_file_contents == '') {
+      $chain_file = ''
+    } else {
+      if $ssl_chain_file == '' {
+        $chain_file = "/etc/ssl/certs/${::fqdn}_intermediate.pem"
+      } else {
+        $chain_file = $ssl_chain_file
+      }
+      if $ssl_chain_file_contents != '' {
+        file { $chain_file:
+          ensure  => present,
+          owner   => 'root',
+          group   => 'root',
+          mode    => '0644',
+          content => $ssl_chain_file_contents,
+          require => File['/etc/ssl/certs'],
+          before  => File[$cert_file],
+        }
+      }
+    }
+
+    ::httpd::vhost { "logs.${domain}":
+      port       => 443, # Is required despite not being used.
+      docroot    => '/srv/static/logs',
+      priority   => '50',
+      ssl        => true,
+      template   => 'openstackci/logs.vhost.erb',
+      vhost_name => "logs.${domain}",
+      require    => [
+        File['/srv/static/logs'],
+        File[$cert_file],
+        File[$key_file],
+      ],
+    }
+
+  } else {
+
+    ::httpd::vhost { "logs.${domain}":
+      port       => 80, # Is required despite not being used.
+      docroot    => '/srv/static/logs',
+      priority   => '50',
+      template   => 'openstackci/logs.vhost.erb',
+      vhost_name => "logs.${domain}",
+      require    => File['/srv/static/logs'],
+    }
+
   }
 
   ::httpd::vhost { "logs-dev.${domain}":
-    port     => 80,
-    priority => '51',
+    port     => 80, # Is required despite not being used.
     docroot  => '/srv/static/logs',
-    require  => File['/srv/static/logs'],
+    priority => '51',
     template => 'openstackci/logs-dev.vhost.erb',
+    vhost_name => "logs-dev.${domain}",
+    require  => File['/srv/static/logs'],
   }
 
   if ! defined(File['/srv/static']) {
