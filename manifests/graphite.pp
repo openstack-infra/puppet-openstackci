c7b8ecd3e9e16c2fb9517bec53e35d738995fd7e
John Warren <jswarren@us.ibm.com>
2015-11-09 10:57:18 -0500

    Add generic graphite manifest
    Adding graphite manifest as part of the reuse refactoring efforts
    as documented at:
    
    https://review.openstack.org/#/c/239810/2/specs/openstackci-extension.rst
    
    Change-Id: I125d58ad4bd3ad4176d7876355a64063f55af849

---
+++ manifests/graphite.pp
@@ -0,0 +1,17 @@
+# == Class: openstackci::graphite
+#
+class openstackci::graphite (
+  $graphite_admin_user,
+  $graphite_admin_email,
+  $graphite_admin_password,
+  $vhost_name,
+) {
+
+  class { '::graphite':
+    graphite_admin_user     => $graphite_admin_user,
+    graphite_admin_email    => $graphite_admin_email,
+    graphite_admin_password => $graphite_admin_password,
+    vhost_name              => $vhost_name,
+  }
+
+}
