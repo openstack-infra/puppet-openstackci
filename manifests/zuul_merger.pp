151ac22772a2adbcef8019077d695f679f5c94d7
Andrey Nikitin <anikitin@mirantis.com>
2016-03-21 17:20:36 +0300

    Order of the classes parameters is refactored
    Order and indentation of those parameters are changed
    to follow Puppet Style Guide recommendation [0].
    Moreover, it will allow to an user to find much faster
    a variable in a list of variables.
    
    Some of parameters are ordered into the groups, to save
    their a project-related puprose.
    
    [0]. https://docs.puppetlabs.com/guides/style_guide.html
    
    Change-Id: I98bc57c6e3a2d8cb7b292046b11de38c413c1ad5

--- manifests/zuul_merger.pp
+++ manifests/zuul_merger.pp
@@ -17,18 +17,18 @@
 # == Class: openstackci::zuul_merger
 #
 class openstackci::zuul_merger(
-  $vhost_name = $::fqdn,
-  $gearman_server = '127.0.0.1',
-  $gerrit_server = '',
-  $gerrit_user = '',
-  $known_hosts_content = '',
+  $gearman_server       = '127.0.0.1',
+  $gerrit_server        = '',
+  $gerrit_user          = '',
+  $git_email            = 'zuul@domain.example',
+  $git_name             = 'Zuul',
+  $git_source_repo      = 'https://git.openstack.org/openstack-infra/zuul',
+  $known_hosts_content  = '',
+  $manage_common_zuul   = true,
+  $revision             = 'master',
+  $vhost_name           = $::fqdn,
   $zuul_ssh_private_key = '',
-  $zuul_url = "http://${::fqdn}/p",
-  $git_email = 'zuul@domain.example',
-  $git_name = 'Zuul',
-  $manage_common_zuul = true,
-  $revision = 'master',
-  $git_source_repo = 'https://git.openstack.org/openstack-infra/zuul',
+  $zuul_url             = "http://${::fqdn}/p",
 ) {
 
   if $manage_common_zuul {
