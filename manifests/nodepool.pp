f36b0330161b48a2a115bb02eb3df9c4ff8664f3
Mateusz Matuszkowiak <mmatuszkowiak@mirantis.com>
2016-04-08 10:57:50 +0200

    Include nodepool::mysql from openstackci
    Since mysql stuff was moved from nodepool class
    into a dedicated one, nodepool::mysql, it is required
    to call it from openstackci::nodepool.
    
    Change-Id: I9aa710614a86ab8019ec1a3e5bc4cf0a70cab549

--- manifests/nodepool.pp
+++ manifests/nodepool.pp
@@ -33,6 +33,13 @@ class openstackci::nodepool (
   $logging_conf_template = 'nodepool/nodepool.logging.conf.erb',
   $builder_logging_conf_template = 'nodepool/nodepool-builder.logging.conf.erb',
   $jenkins_masters = [],
+  $install_mysql = true,
+  $mysql_bind_address = '127.0.0.1',
+  $mysql_default_engine = 'InnoDB',
+  $mysql_db_name = 'nodepool',
+  $mysql_max_connections = 8192,
+  $mysql_user_host_access = 'localhost',
+  $mysql_user_name = 'nodepool',
 ) {
 
   if ! defined(Class['project_config']) {
@@ -41,7 +48,20 @@ class openstackci::nodepool (
     }
   }
 
-  class { '::nodepool':
+  if($install_mysql) {
+    class { '::nodepool::mysql' :
+      $mysql_bind_address     => $mysql_bind_address,
+      $mysql_default_engine   => $mysql_default_engine,
+      $mysql_db_name          => $mysql_db_name,
+      $mysql_max_connections  => $mysql_max_connections,
+      $mysql_root_password    => $mysql_root_password,
+      $mysql_user_host_access => $mysql_user_host_access,
+      $mysql_user_name        => $mysql_user_name,
+      $mysql_user_password    => $mysql_password,
+    }
+  }
+
+  class { '::nodepool' :
     mysql_root_password           => $mysql_root_password,
     mysql_password                => $mysql_password,
     nodepool_ssh_private_key      => $nodepool_ssh_private_key,
@@ -61,46 +81,48 @@ class openstackci::nodepool (
     jenkins_masters               => $jenkins_masters,
   }
 
-  file { '/etc/nodepool/nodepool.yaml':
+  file { '/etc/nodepool/nodepool.yaml' :
     ensure  => present,
     source  => $yaml_path,
-    owner   => 'nodepool',
-    group   => 'root',
+    owner   => $user,
+    group   => $user,
     mode    => '0400',
     require => [
       File['/etc/nodepool'],
-      User['nodepool'],
+      User[$user],
       Class['project_config'],
     ],
   }
 
-  file { '/home/nodepool/.config':
+  file { '/home/nodepool/.config' :
     ensure  => directory,
-    owner   => 'nodepool',
-    group   => 'nodepool',
+    owner   => $user,
+    group   => $user,
     require => [
-      User['nodepool'],
+      File['/home/nodepool'],
+      User[$user],
     ],
   }
 
-  file { '/home/nodepool/.config/openstack':
+  file { '/home/nodepool/.config/openstack' :
     ensure  => directory,
-    owner   => 'nodepool',
-    group   => 'nodepool',
+    owner   => $user,
+    group   => $user,
     require => [
       File['/home/nodepool/.config'],
+      User[$user],
     ],
   }
 
-  file { '/home/nodepool/.config/openstack/clouds.yaml':
+  file { '/home/nodepool/.config/openstack/clouds.yaml' :
     ensure  => present,
-    owner   => 'nodepool',
-    group   => 'nodepool',
+    owner   => $user,
+    group   => $user,
     mode    => '0400',
     content => $oscc_file_contents,
     require => [
       File['/home/nodepool/.config/openstack'],
-      User['nodepool'],
+      User[$user],
     ],
   }
 
