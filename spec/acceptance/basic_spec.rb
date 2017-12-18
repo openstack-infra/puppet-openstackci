054bf5f53cb953d70583b7dc867ce8fb836b045c
Javier Pena <jpena@redhat.com>
2017-12-18 16:25:00 +0100

    Add AFS mirror support
    This commit adds support for the AFS mirror code, taken from [1].
    It also includes some additions:
    
    - Support for CentOS/Red Hat nodes.
    - Acceptance test included for both Debian and Red Hat nodes. Until
      the logserver code supports Red Hat, we are using two separate
      manifests.
    
    [1] - https://github.com/openstack-infra/system-config/blob/master/modules/openstack_project/manifests/mirror.pp
    
    Change-Id: I8598f04b7462a33124c28049063546b90c950332

--- spec/acceptance/basic_spec.rb
+++ spec/acceptance/basic_spec.rb
@@ -25,6 +25,9 @@ describe 'basic openstackci' do
         it { should be_installed }
       end
 
+      describe package('apache2-utils') do
+        it { should be_installed }
+      end
     end
 
     context 'files and directories' do
@@ -33,6 +36,49 @@ describe 'basic openstackci' do
         it { should be_file }
         it { should be_owned_by 'root' }
         it { should be_mode 440 }
+      end
+
+      describe file('/var/cache/apache2/proxy') do
+        it { should be_directory }
+        it { should be_owned_by 'www-data' }
+        it { should be_mode 755 }        
+      end
+
+    end
+
+  end
+
+  if fact('osfamily') == 'RedHat'
+
+    context 'default parameters' do
+
+      it 'should work with no errors' do
+
+        base_path = File.dirname(__FILE__)
+        pp_path = File.join(base_path, 'fixtures', 'default_rh.pp')
+        pp = File.read(pp_path)
+
+        # Run it twice and test for idempotency
+        apply_manifest(pp, :catch_failures => true)
+        apply_manifest(pp, :catch_changes => true)
+      end
+
+    end
+
+    context 'installation of packages' do
+
+      describe package('httpd') do
+        it { should be_installed }
+      end
+
+    end
+
+    context 'files and directories' do
+
+      describe file('/var/cache/httpd/proxy') do
+        it { should be_directory }
+        it { should be_owned_by 'apache' }
+        it { should be_mode 755 }        
       end
 
     end
