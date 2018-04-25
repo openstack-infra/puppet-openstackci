92b4531bc52422356385faa12fdce009005f28dd
Paul Belanger <pabelanger@redhat.com>
2018-04-25 14:54:12 -0400

    Revert "Reduce log archive to 30 days"
    With tripleo-test-cloud-rh1 offline, we've gained back some space for
    logs.
    
    This reverts commit 325dd27fd442aa72253d613e9619d9bca606141c.
    
    Change-Id: I76249bdec00c9225971382e7afb43fc68013745b
    Signed-off-by: Paul Belanger <pabelanger@redhat.com>

--- files/log_archive_maintenance.sh
+++ files/log_archive_maintenance.sh
@@ -6,5 +6,5 @@ find -O3 /srv/static/logs/ -depth -not -name robots.txt -not -name lost+found \
     \( -type f -mmin +10 -not -name \*\[.-\]gz -not -name \*\[._-\]\[zZ\] \
         \( -name \*.txt -or -name \*.html -or -name tmp\* \) \
         -exec gzip \{\} \; \) \
-    -o \( -type f -mtime +30 -execdir rm \{\} \; \) \
+    -o \( -type f -mtime +45 -execdir rm \{\} \; \) \
     -o \( -type d -empty -mtime +1 -execdir rmdir {} \; \) \)
